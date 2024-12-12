23119820f255b97a3fad0a251d6bbd3d410a9585
gabrielmeiradev <gabriel_meira10@hotmail.com>
2024-12-12 16:21:05 -0300

    feat: auth

--- server.js
+++ server.js
@@ -1,27 +1,75 @@
+var net = require("net");
+var fs = require("fs");
 
-var net = require('net');
-var fs = require('fs');
+let date = new Date(),
+  size = 0,
+  elapsed,
+  connected = 0,
+  writer,
+  socketToken,
+  wrongToken = false;
+const DEPLOY_TOKEN = "FUCK";
 
-let date = new Date(), size = 0, elapsed;
-let writer = fs.createWriteStream("output.zip");
+var server = net.createServer(function (socket) {
+  socket.on("connect", () => {
+    if (connected > 1) {
+      socket.end();
+    }
 
-var server = net.createServer(function(socket) {
-    socket.on('connect', () => {
-        console.log("Client connected");
-    });
-    socket.on('data', chunk => {
-        size += chunk.length;
-        elapsed = new Date() - date;
-        process.stdout.write(`\r${(size / (1024 * 1024)).toFixed(2)} MB of data was sent. Total elapsed time is ${elapsed / 1000} s`);
-        writer.write(chunk);
-      });
-      
-      socket.on("end", () => {
-        console.log(`\nFinished getting file. speed was: ${((size / (1024 * 1024)) / (elapsed / 1000)).toFixed(2)} MB/s`);
-      });
+    console.log("Client connected");
+    connected++;
+  });
 
+  socket.once("data", (data) => {
+    socketToken = data.toString();
+
+    if (socketToken != DEPLOY_TOKEN) {
+      wrongToken = true;
+      socket.write(
+        "Socket disconnected because the token provided is incorrect"
+      );
+      socket.end();
+      return;
+    }
+  });
+
+  socket.on("data", (chunk) => {
+    if (wrongToken) return;
+
+    if (!writer) {
+      if (fs.existsSync("output.zip")) {
+        fs.unlinkSync("output.zip");
+      }
+      writer = fs.createWriteStream("output.zip");
+    }
+    size += chunk.length;
+    elapsed = new Date() - date;
+    process.stdout.write(
+      `\r${(size / (1024 * 1024)).toFixed(
+        2
+      )} MB of data was sent. Total elapsed time is ${elapsed / 1000} s`
+    );
+    writer.write(chunk);
+  });
+
+  socket.on("end", () => {
+    if (wrongToken) {
+      console.error(
+        "Socket disconnected because the token provided is incorrect"
+      );
+    } else {
+      console.log(
+        `\nFinished getting file. speed was: ${(
+          size /
+          (1024 * 1024) /
+          (elapsed / 1000)
+        ).toFixed(2)} MB/s`
+      );
+    }
+    size = elapsed = connected = 0;
+  });
 });
 
 server.listen(1337, () => {
-    console.log("Server started on port 1337");
+  console.log("Server started on port 1337");
 });
