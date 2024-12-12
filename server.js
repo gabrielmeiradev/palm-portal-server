70f3fcba2b6686b9c3826b79ea4419783f3ea09f
gabrielmeiradev <gabriel_meira10@hotmail.com>
2024-12-12 14:42:24 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+
+var net = require('net');
+var fs = require('fs');
+
+let date = new Date(), size = 0, elapsed;
+let writer = fs.createWriteStream("output.zip");
+
+var server = net.createServer(function(socket) {
+    socket.on('connect', () => {
+        console.log("Client connected");
+    });
+    socket.on('data', chunk => {
+        size += chunk.length;
+        elapsed = new Date() - date;
+        socket.write(`\r${(size / (1024 * 1024)).toFixed(2)} MB`)
+        process.stdout.write(`\r${(size / (1024 * 1024)).toFixed(2)} MB of data was sent. Total elapsed time is ${elapsed / 1000} s`);
+        writer.write(chunk);
+      });
+      
+      socket.on("end", () => {
+        console.log(`\nFinished getting file. speed was: ${((size / (1024 * 1024)) / (elapsed / 1000)).toFixed(2)} MB/s`);
+      });
+
+});
+
+server.listen(1337, () => {
+    console.log("Server started on port 1337");
+});
